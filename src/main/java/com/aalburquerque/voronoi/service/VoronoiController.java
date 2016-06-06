ac392b4bd4f390e5228314e813bba513201db927
aalburquerque <antonio.alburquerque@gmail.com>
2016-06-06 10:44:09 +0200

    Se ha modificado el pom.xml para que se instale correctamente todos los modulso

--- src/main/java/com/aalburquerque/voronoi/service/VoronoiController.java
+++ src/main/java/com/aalburquerque/voronoi/service/VoronoiController.java
@@ -52,22 +52,5 @@ public class VoronoiController {
 		}
 	}
 
-	private class Greeting {
 
-		private final long id;
-		private final String content;
-
-		public Greeting(long id, String content) {
-			this.id = id;
-			this.content = content;
-		}
-
-		public long getId() {
-			return id;
-		}
-
-		public String getContent() {
-			return content;
-		}
-	}
 }
